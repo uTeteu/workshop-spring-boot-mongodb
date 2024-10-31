3624e4eaae8642098d1453c0b4583928e9b8148e
Teteu <matheuskiper96@gmail.com>
2024-10-31 17:08:33 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/teteu/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.teteu.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.teteu.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
