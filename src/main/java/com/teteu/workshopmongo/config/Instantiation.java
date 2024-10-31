5047c843853e02eb935eb5b4b5cd6358821907a1
Teteu <matheuskiper96@gmail.com>
2024-10-31 19:30:47 -0300

    Consulta simples com query methods

--- src/main/java/com/teteu/workshopmongo/config/Instantiation.java
+++ src/main/java/com/teteu/workshopmongo/config/Instantiation.java
@@ -2,7 +2,6 @@ package com.teteu.workshopmongo.config;
 
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
-import java.util.Date;
 import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
--- src/main/java/com/teteu/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/teteu/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.teteu.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,6 @@ import com.teteu.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
+	List<Post> findByTitleContainingIgnoreCase(String txt);
+	
 }
