4ee842d30bf270d6531375468b34221d53cab7a5
Teteu <matheuskiper96@gmail.com>
2024-10-31 19:01:28 -0300

    Adicionando comentarios aos posts

--- src/main/java/com/teteu/workshopmongo/config/Instantiation.java
+++ src/main/java/com/teteu/workshopmongo/config/Instantiation.java
@@ -2,6 +2,7 @@ package com.teteu.workshopmongo.config;
 
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,7 @@ import org.springframework.context.annotation.Configuration;
 import com.teteu.workshopmongo.domain.Post;
 import com.teteu.workshopmongo.domain.User;
 import com.teteu.workshopmongo.dto.AuthorDTO;
+import com.teteu.workshopmongo.dto.CommentDTO;
 import com.teteu.workshopmongo.repository.PostRepository;
 import com.teteu.workshopmongo.repository.UserRepository;
 
@@ -42,6 +44,13 @@ public class Instantiation implements CommandLineRunner {
 							"Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia",
 							"Acordei feliz hoje!", new AuthorDTO(maria));
+		
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
