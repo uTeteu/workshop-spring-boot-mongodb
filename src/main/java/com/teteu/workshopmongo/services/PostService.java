faa4219dd8662526e99bd8ee5cd81e8807c479ce
Teteu <matheuskiper96@gmail.com>
2024-10-31 17:40:35 -0300

    Obtendo post por id

---
+++ src/main/java/com/teteu/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.teteu.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.teteu.workshopmongo.domain.Post;
+import com.teteu.workshopmongo.repository.PostRepository;
+import com.teteu.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
